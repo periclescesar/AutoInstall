4a7ae974869b6f4bd480f8f35e888fd5e37bb182
Péricles Cesar da Costa <periclescesar@gmail.com>
2015-01-06 23:39:35 -0200

    Adicionado o Instalador do GrubCustomizer.

---
+++ controllers/GrubCustomizerInstall.sh
@@ -0,0 +1,5 @@
+#Grub Customizer Install
+sudo add-apt-repository ppa:danielrichter2007/grub-customizer;
+sudo apt-get update;
+sudo apt-get install grub-customizer;
+echo "\n\n\tInstalação Finalizada\n\n";
