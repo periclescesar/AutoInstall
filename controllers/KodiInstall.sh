fe2031caaf4b3a25f4e5350190cc2d231d237dbd
Péricles Cesar da Costa <periclescesar@gmail.com>
2015-01-09 00:38:18 -0200

    Atualizado o instalador do Kodi, Agora Funcionando

--- controllers/KodiInstall.sh
+++ controllers/KodiInstall.sh
@@ -6,5 +6,5 @@ sudo apt-get install software-properties-common;
 sudo add-apt-repository ppa:team-xbmc/ppa;
 sudo apt-get update;
 echo "\n\n\tInstalando Kodi\n\n"
-sudo apt-get install kodi;
+sudo apt-get install kodi kodi-pvr* kodi-audioencoder*;
 echo "\n\n\tInstalação Finalizada\n\n";
