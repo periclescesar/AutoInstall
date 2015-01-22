eb70a5e6b0fe5bc19636472ac8e3e9d1fac31782
Péricles Cesar da Costa <periclescesar@gmail.com>
2015-01-21 22:28:14 -0200

    Foi corrigido a validação da instalação do netbeans

--- controllers/NetBeansInstall.sh
+++ controllers/NetBeansInstall.sh
@@ -6,26 +6,23 @@ cd /tmp
 wget -O netbeans.png http://ubuntuhandbook.org/wp-content/uploads/2014/01/netbeans-logo.png
 
 
-echo "\n\nBaixando NetBeans 8.0...";
+echo "\n\nBaixando NetBeans 8.0.2...";
 wget -O netbeans8.0.2.sh http://download.netbeans.org/netbeans/8.0.2/final/bundles/netbeans-8.0.2-linux.sh;
 
 
-echo "\n\nInstalando NetBeans 8.0...";
+echo "\n\nInstalando NetBeans 8.0.2...";
 sh netbeans8.0.2.sh;
-
-
 
 
 # mensagens finais
 #checando instalação
-isInstall=$(sudo dpkg -l | grep -c "netbeans");
-if [ $isInstall -ge 1 ]; then
+caminho="/home/pericles/netbeans-8.0.2";
+if[! -d "$caminho"]; then
 	echo "\n\n\tInstalação finalizada com sucesso!\n\n";
-	notify-send -u normal -i /tmp/netbeans.png 'NetBeans 8.0' 'Instalação finalizada com sucesso!'
-	rm netbeans8.0.2.sh;
+	notify-send -u normal -i /tmp/netbeans.png 'NetBeans 8.0.2' 'Instalação finalizada com sucesso!'
 else
 	echo "\n\n\tErro na Instalação!\n\n";
-	notify-send -u normal -i /tmp/netbeans.png 'NetBeans 8.0' 'Erro na Instalação!'
+	notify-send -u normal -i /tmp/netbeans.png 'NetBeans 8.0.2' 'Erro na Instalação!'
 fi
 
 # Limpando Cache
