f5a9b99977d1c145232acefa8f1ed19a2cedc389
Péricles Cesar da Costa <periclescesar@gmail.com>
2015-02-09 18:45:44 -0200

    Adicionado a função para configurar o ambiente

--- controllers/ApacheInstall.sh
+++ controllers/ApacheInstall.sh
@@ -7,12 +7,17 @@ sudo apt-get install apache2;
 cd /tmp
 wget -O apache.png http://ubuntuserverguide.com/wp-content/uploads/2012/10/apache-logo.png;
 
+configAmbi() {
 
+
+	notify-send -u normal -i /tmp/mysql.png 'Apache2' 'Ambiente configurado!'
+}
 
 isInstall=$(sudo dpkg -l | grep -c "apache2");
 if [ $isInstall -ge 1 ]; then
 	echo "\n\n\tInstalação finalizada com sucesso!\n\n";
 	notify-send -u normal -i /tmp/apache.png 'Apache2' 'Instalado Com Sucesso!'
+	configAmbi;
 else
 	echo "\n\n\tErro na Instalação!\n\n";
 	notify-send -u normal -i /tmp/apache.png 'Apache2' 'Erro na Instalação!'
