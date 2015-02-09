94714c1eae2252d8497d6ff583795c1d7ad84158
Péricles Cesar da Costa <periclescesar@gmail.com>
2015-02-09 18:36:46 -0200

    Instalador do Mysql atualizado para configurar o ambiente
    O script execulta o criador de estrutura de pastas do Mysql e depois
    execulta um script para deixar o mysql mais seguro.

--- controllers/MysqlInstall.sh
+++ controllers/MysqlInstall.sh
@@ -1,4 +1,16 @@
 # MySQL5.6 Install
+
+#configurando ambiente
+confAmbi() {
+	echo "Criando a estrutura de diretório de banco de dados...";
+	sudo mysql_install_db;
+
+	echo "rodando script de segurança...";
+	sudo mysql_secure_installation;
+
+	notify-send -u normal -i /tmp/mysql.png 'MySQL5.6' 'Ambiente configurado!'
+}
+
 sudo apt-get update;
 echo "\n\n\tInstalando o MySQL5.6...";
 sudo apt-get -y install mysql-server-5.6;
@@ -8,11 +20,13 @@ cd /tmp
 wget -O mysql.png http://www.plustar.jp/lab/blog/wp-content/uploads/2012/06/mysql-logo.png;
 
 
-
 isInstall=$(sudo dpkg -l | grep -c "mysql-server-5.6");
 if [ $isInstall -ge 1 ]; then
 	echo "\n\n\tInstalação finalizada com sucesso!\n\n";
 	notify-send -u normal -i /tmp/mysql.png 'MySQL5.6' 'Instalado Com Sucesso!'
+	echo "configurando ambiente..."
+	confAmbi;
+	
 else
 	echo "\n\n\tErro na Instalação!\n\n";
 	notify-send -u normal -i /tmp/mysql.png 'MySQL5.6' 'Erro na Instalação!'
