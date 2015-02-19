8689ea4a4cda7905fab0a53e3820611e79931cd9
Péricles Cesar da Costa <periclescesar@gmail.com>
2015-02-19 11:15:07 -0200

    Atualizado a instalação LAMP.
    Atualizado a chamada dos SHs para a pasta raiz.
    Adicionado o yes to all no apt-get.
    Adicionado a criação do arquivo phpinfo na pasta html e
    a abertura do mesmo ao finalizar a instalação.

--- controllers/PackLAMPInstall.sh
+++ controllers/PackLAMPInstall.sh
@@ -1,23 +1,31 @@
 # Pacote de Desenvolvimento WEB (LAMP)
 echo "Iniciando o instalador do Apache...";
-sh ApacheInstall.sh;
+sh controllers/ApacheInstall.sh;
 
 echo "Iniciando o instalador do MySQL...";
-sh MysqlInstall.sh;
+sh controllers/MysqlInstall.sh;
 
 echo "Iniciando o instalador do PHP...";
-sh PhpInstall.sh;
+sh controllers/PhpInstall.sh;
 
 echo "Instalando extenção libapache2-mod-php5...";
-sudo apt-get install libapache2-mod-php5;
+sudo apt-get -y install libapache2-mod-php5;
 
 echo "Instalando extenção php5-mysql...";
-sudo apt-get install php5-mysql;
+sudo apt-get -y install php5-mysql;
 
 #Criando grupo de usuarios "Desenvolvedor Web"
 sudo groupadd Desenvolvedor_Web;
 sudo adduser $(whoami) Desenvolvedor_Web;
 sudo chgrp -R Desenvolvedor_Web /var/www/
 sudo chmod -R g+rwx /var/www/;
+# tem q usar umasck 
+sudo umasck 003 /var/www/;
+sudo chmod -R g+s /var/www/;
+
+
+sudo echo '<?php phpinfo();?>' >> /var/www/html/phpInfo.php
+
+xdg-open http://localhost/phpInfo.php
 
 
